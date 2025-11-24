c88142436b60d43068c8e3efc0be98cfd73daba9
merkle@mexc.com <merkle@mexc.com>
2025-11-24 09:51:45 +0800

    # fix

---
+++ src/main/java/com/mexc/merkle/data/config/MerkleDataSourceConfig.java
@@ -0,0 +1,61 @@
+package com.mexc.merkle.data.config;
+
+import com.mexc.merkle.data.util.AwsSecretManager;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.Resource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Import(MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar.class)
+@MapperScan(basePackages = "com.mexc.merkle.data.mapper", sqlSessionFactoryRef = "merkleSqlSessionFactory")
+@EnableTransactionManagement
+@Slf4j
+public class MerkleDataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("classpath*:mapper/*.xml")
+    private Resource[] mapperLocations;
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean("merkleDataSource")
+    public DataSource getDataSource() {
+        AwsSecretManager awsSecretManager = new AwsSecretManager(awsRegion);
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        String secretUrl = awsSecretManager.getSecretField(url);
+        String secretUserName = awsSecretManager.getSecretField(username);
+        String secretPassword = awsSecretManager.getSecretField(password);
+        return dataSourceBuilder.url(StringUtils.hasText(secretUrl) ? awsSecretManager.getValue(secretUrl) : url)
+                .username(StringUtils.hasText(secretUserName) ? awsSecretManager.getValue(secretUserName) : username)
+                .password(StringUtils.hasText(secretPassword) ? awsSecretManager.getValue(secretPassword) : password)
+                .type(HikariDataSource.class).build();
+    }
+
+    @Bean("merkleSqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactoryBean(@Qualifier("merkleDataSource") DataSource ds) {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(ds);
+        bean.setTypeAliasesPackage("com.mexc.merkle.data.mapper");
+        bean.setMapperLocations(mapperLocations);
+        return bean;
+    }
+
+}
---
+++ src/main/java/com/mexc/merkle/data/util/AwsSecretManager.java
@@ -0,0 +1,64 @@
+package com.mexc.merkle.data.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.springframework.util.StringUtils;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * aws secret manager
+ */
+@Slf4j
+public class AwsSecretManager {
+    String secretName = "mxc.merkle";
+    Region region = null;
+    // Create a Secrets Manager client
+    SecretsManagerClient client = SecretsManagerClient.builder()
+            .region(region)
+            .build();
+    GetSecretValueResponse getSecretValueResponse = null;
+    Map<String, String> map = new HashMap<>();
+
+    public AwsSecretManager(String regionName) {
+        try {
+            region = Region.of(regionName);
+            GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                    .secretId(secretName)
+                    .build();
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, String> readValue = objectMapper.readValue(getSecretValueResponse.secretString(), new TypeReference<Map<String, String>>() {
+            });
+            if (MapUtils.isNotEmpty(readValue)) {
+                map.putAll(readValue);
+            } else {
+                log.error("getSecretValueResponse,value failed value:{}", getSecretValueResponse.secretString());
+            }
+        } catch (Throwable e) {
+            log.error("get aws secret error", e);
+        }
+    }
+
+    public String getValue(String key) {
+        return map.getOrDefault(key, null);
+    }
+
+    public String getSecretField(String key) {
+        if (!StringUtils.hasText(key)) {
+            return null;
+        }
+        if (key.matches("config\\(([^)]+)\\)")) {
+            String value = key.replaceAll("config\\(([^)]+)\\)", "$1");
+            return value.replace(secretName, "").replaceFirst(".", "");
+        }
+        return null;
+    }
+}
